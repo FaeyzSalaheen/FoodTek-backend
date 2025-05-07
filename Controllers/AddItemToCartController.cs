fe675ecc8f71eec8fd059c99147dd5ccea75d164
faeyz <160082475+Faeyz200200@users.noreply.github.com>
2025-05-07 04:56:53 +0300

    Add Item To Cart Controller

---
+++ Controllers/AddItemToCartController.cs
@@ -0,0 +1,33 @@
+﻿using Foodtek.DTOs.AddItemToCart;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Foodtek.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        [HttpPost("[action]")]
+        public async Task<IActionResult> AddItemToCart(AddItemInput input)
+        {
+            try
+            {
+
+
+                throw new NotImplementedException("This method is not implemented yet.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    new AddItemOutput
+                    {
+                        StatusCode = -1,
+                        Message = $"Internal server error: {ex.Message}"
+                    });
+            }
+        }
+    }
+}
---
+++ DTOs/AddItemToCart/AddItemOutput.cs
@@ -0,0 +1,9 @@
+﻿namespace Foodtek.DTOs.AddItemToCart
+{
+    public class AddItemOutput
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public int? CartId { get; set; }
+    }
+}
