1f5e2215307a85b57dbf23bd73a3d1ed7e64d0bb
FaeyzSalaheen <faez.alabadi@gmail.com>
2025-05-17 10:43:47 +0300

    fixing

--- Helpers/OTP/OTPHelper.cs
+++ Helpers/OTP/OTPHelper.cs
@@ -7,14 +7,14 @@ using SendGrid;
 public static class EmailService
 {
     public static async Task SendEmail(string email , string messag , string code)
-    {
-        var apiKey = "Ae8V3VA7TW6r-nQ3ou3eOg";
+    {  
+          
+        var apiKey = "";
         var client = new SendGridClient(apiKey);
         var from = new EmailAddress("foodtekservis@gmail.com", "FoodTek");
         var subject = "FoodTek";
         var to = new EmailAddress(email, "e");
         var plainTextContent = $"{messag}{code}";
-        //var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
         var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent,"");
         var response = await client.SendEmailAsync(msg);
     }
